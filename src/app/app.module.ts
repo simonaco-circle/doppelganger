8dbb7bebce2acfa810427e11e74d514e40e70183
Simona Cotin <simona.cotin@gmail.com>
2018-03-22 20:47:00 +0000

    Move service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { FaceComponent } from './face/face.component';
-import { FaceService } from './face.service';
+import { FaceService } from './face/face.service';
 
 @NgModule({
   declarations: [AppComponent, FaceComponent],
